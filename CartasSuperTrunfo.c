853f9fa7c7e1621f0631eca36317e726ccddd936
j-alexandre-magalhaes-debug <j-alexandre-magalhaes@bol.com.br>
2025-09-13 20:27:54 +0000

    fiz carta de trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,42 @@
-#include <stdio.h>
+#include <stdio.h>{
+ 
+//carta suoer trunfo
+  
+  char:rio de janeiro
+  char:Código [Av1]
+  char:Cidade [rio de janeiro ]
+  int : população 1000000
+}
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+Voidlercarta(StructCartaSuperTrunfo){
+  
+  printf("\nRio de janeiro(A):");
+  scanf("%c", Carta-> Estado);
+  printf("Av1:");
+  scanf("%s",carta-> codigo);
+  printf("Rio janeiro:");
+  scanf("%s", carta -> cidade);
+  printf("1000000:");
+  scanf("%d",&carta -> população);
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  int main (){
 
+  struct carta super trunfo carta1, carta2;
+  
+  printf("Rio de janeiro:%c \n:Av1 %s\n Rio de janeiro %s\n 1000000:%d\n"); 
+  ler carta (&carta2);
+
+  Printf("\ndados da segunda carta:\n");
+  printf("Minas gerais:%c\n Av2:%s\n Belo Horizonte:%s\n 200000:%d\n"),carta 2.estado, carta 2.codigo,carta 2.cidade, carta 2.populacao);
+
+  }
+ 
+  return 0;
+
+} __clang_literal_encoding__
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
 
-return 0;
-} 
+
+
